bbca12d00730776168943b0fa99d09f1ba20a35a
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-12-18 12:41:53 -0300

    chore: add new elements

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
-  return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'));
+	return gulp.src('./src/styles/*.scss') //Aqui é onde vamos pegar os arquivos fonte.
+		.pipe(sass({ outputStyle: 'compressed' })) //Os arquivos de saida serão comprimidos.
+		.pipe(gulp.dest('./dist/css')); // Aqui vai ser o destino dos arquivos comprimidos.
 }
 
-exports.default = styles;
+function images(){
+	return gulp.src('./src/images/**/*') //Aqui é onde vamos pegar os arquivos fonte.
+		.pipe(imagemin()) 
+		.pipe(gulp.dest('./dist/images')); // Aqui vai ser o destino dos arquivos comprimidos.
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+	gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
